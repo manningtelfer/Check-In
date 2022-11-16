e5290ae6aec7c6358ce3c519fff3c0e658c03ed9
manningtelfer <manning.telfer@me.com>
2022-11-16 23:25:18 +1100

    Cleanup defaults in preparation for whitelabel sup

--- include/component/footer.php
+++ include/component/footer.php
@@ -1,4 +1,4 @@
 <?php $app = parse_ini_file('app.ini'); $url_privacy = $app['url_privacy'];?>
 <div class="container">
-    <p>Your information will be used to log your visit to our office for security purposes. Any information you submit will be handled in accordance with our <a href="<?php echo $url_privacy ?>" target="_blank">Privacy Policy</a>.</p>
+    <p>Your information will be used to log your visit for security purposes and will be handled in accordance with our <a href="<?php echo $url_privacy ?>" target="_blank">Privacy Policy</a>.</p>
 </div>
